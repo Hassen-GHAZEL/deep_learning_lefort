28d7304c31adb90542f1af71fc5a4a0bc43f7138
GHAZEL HASSEN p1908632 <p1908632@univ-lyon1.fr>
2024-10-03 09:54:50 +0200

    3h d'execution

--- stop_process.py
+++ stop_process.py
@@ -1,7 +1,7 @@
 import time
 from tools import enregistrer_fin_programme,lire_pid_du_fichier,git_commit_and_push, shutdown_system
 
-time.sleep(1)
+time.sleep(3*3600)
 enregistrer_fin_programme(lire_pid_du_fichier())
 git_commit_and_push("commit from python execution, après 1sec d'exécution")
 shutdown_system()
