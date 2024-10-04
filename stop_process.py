da2f12703dbc5e650f7599f31c97d6500b3eaeaf
GHAZEL HASSEN p1908632 <p1908632@univ-lyon1.fr>
2024-10-04 14:10:31 +0200

    attente de 1H30

--- stop_process.py
+++ stop_process.py
@@ -1,7 +1,7 @@
 import time
 from tools import enregistrer_fin_programme,lire_pid_du_fichier,git_commit_and_push, shutdown_system
 
-time.sleep(1*3600) # 1H00
+time.sleep(2*3600+30*60) # 1H30
 enregistrer_fin_programme(lire_pid_du_fichier())
-git_commit_and_push("commit from python execution, après 1H00 d'exécution")
+git_commit_and_push("commit from python execution, après 1H30 d'exécution")
 shutdown_system()
