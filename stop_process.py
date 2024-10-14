115096357a2028b378307557bebd90ac827958de
hassen <hassen.ghazel@etu.univ-lyon1.fr>
2024-10-14 05:40:10 +0200

    commit from python execution, après 4h30 d'exécution

--- stop_process.py
+++ stop_process.py
@@ -1,7 +1,7 @@
 import time
 from tools import enregistrer_fin_programme,lire_pid_du_fichier,git_commit_and_push, shutdown_system
 
-time.sleep(5*3600) # 5H05
+time.sleep(4*3600 + 30*60) # 4h30
 enregistrer_fin_programme(lire_pid_du_fichier())
-git_commit_and_push("commit from python execution, après 05h00 d'exécution")
+git_commit_and_push("commit from python execution, après 4h30 d'exécution")
 shutdown_system()
